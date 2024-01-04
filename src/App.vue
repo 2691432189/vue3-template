7b3e1f527e19976ec943cb4ab954020ddbfed18b
koufengqi <2691432189@qq.com>
2024-01-04 15:11:35 +0800

    单独引入弹框类样式

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,11 @@
 <script setup lang="ts">
 import 'animate.css'
 import 'normalize.css'
+// 自动引入组件，需单独引入element-plus弹框类样式
+import "element-plus/theme-chalk/el-loading.css"
+import "element-plus/theme-chalk/el-message.css"
+import "element-plus/theme-chalk/el-notification.css"
+import "element-plus/theme-chalk/el-message-box.css"
 </script>
 
 <style lang="scss">
